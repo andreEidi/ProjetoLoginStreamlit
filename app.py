c36d5d8e855cf40ee8d8e429844e2cc9ba0c894e
Andre Eidi Maeda <andre_eidi_maeda@outlook.com>
2025-06-26 19:56:46 -0300

    My first commit

---
+++ app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+from time import sleep
+
+def config():
+    # Configuration for the authenticator
+    try:
+        consulta_geral()
+    except:
+        cria_tabela()
+
+    db_query = consulta_geral()
+
+    registros = {'usernames': {}}
+    for data in db_query:
+        registros['usernames'][data[1]] = {'name' : data[0], 'password' : data[2]}
+
+    COOKIE_EXPIRY_DAYS = 30
+    authenticator = stauth.Authenticate(
+        registros,
+        'my_cookie',
+        'key_cookie',
+        COOKIE_EXPIRY_DAYS,
+    )
+    return authenticator
+
+def main():
+    authenticator = config()
+
+    if 'clicou_registrar' not in st.session_state:
+        st.session_state['clicou_registrar'] = False
+
+    if st.session_state['clicou_registrar'] == False:
+        login_form(authenticator=authenticator)
+    else:
+        usuario_form()
+    
+
+def login_form(authenticator):
+    name, authentication_status, username = authenticator.login('Login')
+    if authentication_status:
+        st.success(f'Bem-vindo {name}')
+        sleep(1)
+        st.session_state['clicou_registrar'] = True
+        st.experimental_rerun()
+    elif authentication_status is False:
+        st.error('Usuário ou senha incorretos')
+    elif authentication_status is None:
+        st.warning('Por favor, faça o login')
+
+
